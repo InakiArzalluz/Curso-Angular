eb6c2d4738a057b7c85ac2ba22e27d21c8e5ae68
Iñaki Arzalluz <inoooaak@gmail.com>
2024-08-07 19:11:18 -0300

    interactuo con los request al interceptarlos (de salida)

--- 22-http-start/src/app/app.module.ts
+++ 22-http-start/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth-Interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, // va a interceptarlos Automaticamente a TODOS
+      useClass: AuthInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
