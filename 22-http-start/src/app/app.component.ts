e847ef81048dd3f29a309fd60caba1d3e3a9fc5a
Iñaki Arzalluz <inoooaak@gmail.com>
2024-08-07 16:41:07 -0300

    Avance http

---
+++ 22-http-start/src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.onFetchPosts();
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.http.post<{ name: string }>(
+      'https://ng-complete-guide-3b842-default-rtdb.firebaseio.com/posts.json',
+      postData,
+    ).subscribe(responseData =>{ console.log(responseData); });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.isFetching = true;
+    this.http
+      .get<{ [key: string]: Post }>('https://ng-complete-guide-3b842-default-rtdb.firebaseio.com/posts.json')
+      .pipe(
+        map(responseData => {
+        const postsArray: Post[] = [];
+        for (const key in responseData){
+          if (responseData.hasOwnProperty(key)){
+            postsArray.push( {...responseData[key], id:key} );
+            // llaves crean nuevo objeto, "..." es spread operator, expande un iterable
+          }
+        }
+        return postsArray;
+        })
+      ).subscribe(posts => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      });
+  }
+
+  onClearPosts() {
+    // Send Http request
+    
+  }
+}
